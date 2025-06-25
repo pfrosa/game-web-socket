ea9fa6353f8cf1dbd73f3f93155846491472689e
Pablo Flavio <pfrosa64@gmail.com>
2025-06-25 01:20:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,32 +3,106 @@ const http = require('http');
 
 const PORT = process.env.PORT || 3000;
 
-// Criar servidor HTTP
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("Servidor WebSocket rodando");
+    res.writeHead(200);
+    res.end("WebSocket running");
 });
 
-// Criar servidor WebSocket usando o servidor HTTP
+const MESSAGE_TYPES = {
+    CONNECT: 'connect',
+    STATE_UPDATE: 'state_update',
+}
+
+const ROOMS = {};
+
+const heartbeat = () => {
+    this.isAlive = true;
+}
+
+const handleMessage = (client, messageJson) => {
+    if (messageJson.messageType == MESSAGE_TYPES.CONNECT) {
+        if (ROOMS[messageJson.room]) {
+            ROOMS[messageJson.room].connectedClients.push(client);
+            if(ROOMS[messageJson.room].connectedClients.length > 1){
+                client.send(JSON.stringify({
+                    messageType: MESSAGE_TYPES.STATE_UPDATE,
+                    gameState: { player: 'black'}
+                }));
+            }
+        } else {
+            ROOMS[messageJson.room] = {
+                connectedClients: [client],
+                gameState: {}
+            }
+        }
+    }
+
+    if (messageJson.messageType == MESSAGE_TYPES.STATE_UPDATE) {
+        //UPDATE ALL OTHER CLIENTS WITH THE STATE 
+        const currentRoom = ROOMS[messageJson.room];
+        currentRoom.gameState = messageJson.gameState;
+        currentRoom.connectedClients.forEach((ws) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                ws.send(JSON.stringify({
+                    messageType: MESSAGE_TYPES.STATE_UPDATE,
+                    gameState: currentRoom.gameState
+                }));
+            }
+        });
+    }
+
+
+    console.log(ROOMS);
+}
+
+
 const wss = new WebSocket.Server({ server });
+wss.on('connection', (ws, req) => {
+    const ip = req.socket.remoteAddress;
+    ws.isAlive = true;
+    ws.ip = ip;
 
-// Lidar com conexões
-wss.on('connection', (ws) => {
-  console.log('Cliente conectado');
+    //add id as unique identifier
+    ws.on('error', console.error);
 
-  ws.on('message', (message) => {
-    console.log(`Mensagem recebida: ${message}`);
-    ws.send(`Você disse: ${message}`);
-  });
+    console.log('Client conected');
 
-  ws.on('close', () => {
-    console.log('Cliente desconectado');
-  });
+    ws.on('message', (message) => {
+        //console.log(wss.clients); // all clients
+        try {
+            const messageJson = JSON.parse(message);
+            handleMessage(ws, messageJson);
+        } catch (error) {
+            console.error(error)
+        }
+    });
 
-  ws.send('Bem-vindo ao WebSocket!');
+    //Check for inative clients
+    const interval = setInterval(function ping() {
+        wss.clients.forEach((ws) => {
+            if (ws.isAlive === false) return ws.terminate();
+            ws.isAlive = false;
+            ws.ping();
+        });
+    }, 30000);
+
+    ws.on('close', () => {
+        //Remove itself from rooms on disconnect
+        Object.values(ROOMS).forEach(room => {
+            const indexOfClient = room.connectedClients.indexOf(ws);
+            if (indexOfClient !== -1) {
+                room.connectedClients.splice(indexOfClient, 1);
+            }
+        })
+        console.log('Client desconected');
+    });
+
+    ws.on('pong', heartbeat);
+    clearInterval(interval);
 });
+
 
 // Iniciar o servidor
 server.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Server running on port:${PORT}`);
 });
